76fee803d051eb87fbf723509e34750d6a716615
Tomas Pesek <pesekt@gmail.com>
2023-11-30 20:21:41 +0100

    updated script

--- docker-scripts.ps1
+++ docker-scripts.ps1
@@ -1,9 +1,9 @@
 docker run -d -p 9200:9200 -e "discovery.type=single-node" `
--v esdata:/usr/share/elasticsearch/data `
+-v esdata:/usr/share/elasticsearch/data --name elasticsearch `
 docker.elastic.co/elasticsearch/elasticsearch:7.9.3
 
-docker run -d -p 15672:15672 -p 5672:5672 masstransit/rabbitmq
+docker run -d -p 15672:15672 -p 5672:5672 --name masstransit-rabbitmq masstransit/rabbitmq
 
 docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Pass@word1' -p 1433:1433 `
 -v sqlvolume:/var/opt/mssql `
--d mcr.microsoft.com/mssql/server:2019-latest
+-d --name mssql-2019 mcr.microsoft.com/mssql/server:2019-latest
